204a47e3d475da873bfb7267af74075aa05405fc
Richard Allen <rsaxvc@gmail.com>
2026-01-12 18:11:44 -0600

    command line arguments

--- jFloaty.cpp
+++ jFloaty.cpp
@@ -12,8 +12,8 @@
 static int desired_channels = 1;
 
 static int fltVecCmp(const void* l_, const void* r_) {
-  const float *l = (const float*)l_;
-  const float *r = (const float*)r_;
+  const float* l = (const float*)l_;
+  const float* r = (const float*)r_;
   for (int i = 0; i < desired_channels; ++i) {
     if (r[i] > l[i]) return 1;
     if (l[i] > r[i]) return-1;
@@ -21,118 +21,253 @@ static int fltVecCmp(const void* l_, const void* r_) {
   return 0;
 }
 
+static void idct_test() {
+  std::cout << "running inverse DCT comparison" << std::endl;
+  for (unsigned i = 0; i < 10; ++i) {
+    short idct_input[64];
+    for (unsigned j = 0; j < 64; ++j) {
+      idct_input[j] = rand() % 64 - 32;
+    }
 
-int main()
+    stbi_uc out_fix[64];
+    stbi__idct_block(out_fix, 8, idct_input);
+
+    float out_f32[64];
+    stbi__idct_block_f32(out_f32, 8, idct_input);
+
+    float maxDiff = 0;
+    for (unsigned j = 0; j < 64; ++j) {
+      float diff = fabsf(out_fix[j] / 255.0f - out_f32[j]);
+      if (diff > maxDiff) {
+        maxDiff = diff;
+      }
+    }
+
+    std::cout << "\tRun:" << i << " maxDiff:" << maxDiff << std::endl;
+  }
+}
+
+static void usage(const char* progName) {
+  std::cerr << progName << ": IEEE 32-bit Floating-Point JPEG codec" << std::endl;
+  std::cerr << std::endl;
+  std::cerr << "Mental Model:" << std::endl;
+  std::cerr << "\tArguments are processed in left to right order." << std::endl;
+  std::cerr << "\tThe program works with one image-buffer at a time." << std::endl;
+  std::cerr << "\tLoading populates the image-buffer." << std::endl;
+  std::cerr << "\tStoring writes the image-buffer to a file." << std::endl;
+  std::cerr << std::endl;
+  std::cerr << "Error handling: immediate, and fatal" << std::endl;
+  std::cerr << std::endl;
+  std::cerr << "Arguments:" << std::endl;
+  std::cerr << "\t-i <filename> #load file into buffer" << std::endl;
+  std::cerr << "\t-s #generate some statistics" << std::endl;
+  std::cerr << "\t-o <filename> #safe buffer to file" << std::endl;
+  std::cerr << std::endl;
+  std::cerr << "\t-idct #run idct self test" << std::endl;
+  std::cerr << std::endl;
+  std::cerr << "\t-q 0-99 #Set JPEG encoder quality factor" << std::endl;
+}
+
+static bool strEndsWith(const char* haystack, const char* needle) {
+  const auto haystackLen = strlen(haystack);
+  const auto needleLen = strlen(needle);
+  if (haystackLen < needleLen) {
+    return false;
+  }
+  return 0 == strcmp(haystack + haystackLen - needleLen, needle);
+}
+
+static void stats(const float* buf, size_t n, int c) {
+  float * pixels = (float*)malloc(sizeof(float) * n * c);
+  memcpy(pixels, buf, sizeof(float) * n * c);
+  std::cout << "Total pixels:" << n << std::endl;
+  std::cout << "Unique pixels:";
+  qsort(pixels, n, sizeof(float) * c, fltVecCmp);
+  unsigned pcount = 1;
+  for (size_t i = 1; i < n; ++i) {
+    const float* pixel = pixels + i * c;
+    pcount += !memcmp(pixel, pixel - c, sizeof(float) * c);
+  }
+  std::cout << pcount << std::endl;
+  std::cout << "Unique/Total Ratio:" << 100 * pcount / n << "%" << std::endl;
+
+  for (int i = 0; i < c; ++i) {
+    std::cout << "ch" << i << ":";
+    float fmin = INFINITY;
+    float fmax = -INFINITY;
+    for (size_t j = i; j < n * c; j += c) {
+      float pixel = buf[j];
+      fmin = std::min(pixel, fmin);
+      fmax = std::max(pixel, fmax);
+    }
+    std::cout << "[" << fmin << "," << fmax << "]/1" << " ";
+    std::cout << "[" << (int)(fmin * 255.0f) << ", " << (int)(255.0f * fmax + .999f) << "] / 255";
+    std::cout << std::endl;
+  }
+
+  free(pixels);
+}
+
+
+int main(int nArgs, const char* args[])
 {
+  float* buffer = NULL;
 
-#if 0
-    for (unsigned i = 0; i < 10; ++i) {
-        short idct_input[64];
-        for (unsigned j = 0; j < 64; ++j) {
-            idct_input[j] = rand() % 64 - 32;
-        }
+  if (nArgs == 1) {
+    usage(args[0]);
 
-        stbi_uc out_fix[64];
-        stbi__idct_block(out_fix, 8, idct_input);
+    std::cerr << "No arguments detected, trying to process test files" << std::endl;
 
-        float out_f32[64];
-        stbi__idct_block_f32(out_f32, 8, idct_input);
+    static const char* dummyArgs[] = { 
+      args[0], "-h", "-idct",
+      "-i", "input1.jpg", "-s",
+      "-o", "output1.fff.data",
+      "-o", "output1.jpg",
+      "-i", "input2.fff.data",
+      "-o", "output2.fff.data",
+      "-o", "output2.jpg"
+    };
+    args = dummyArgs;
+    nArgs = sizeof(dummyArgs) / sizeof(dummyArgs[0]);
+  }
 
-        float maxDiff = 0;
-        for (unsigned j = 0; j < 64; ++j) {
-            float diff = fabsf(out_fix[j] / 255.0f - out_f32[j]);
-            if (diff > maxDiff) {
-                maxDiff = diff;
-            }
-        }
+  int w = -1;
+  int h = -1;
+  int c = -1;
+  int q = 99;
 
-        std::cout << "Run:" << i << " maxDiff:" << maxDiff << std::endl;
+  for (int arg = 1; arg < nArgs; ++arg) {
+    bool more = arg + 1 < nArgs;
+
+    if (!strcmp(args[arg], "-h")) {
+      usage(args[0]);
     }
-#endif
-
-#if 0
-    // --- Reading from a file (mode: "r" for read) ---
-    FILE* fp = fopen("input.jpg", "rb");
-    if (fp == NULL) {
-        printf("Error opening file for reading!\n");
+    else if (!strcmp(args[arg], "-q") && more) {
+      arg++;
+      q = strtol(args[arg], NULL, 10);
+      std::cout << "JPEG encoder quality factor set to " << q << std::endl;
+    }
+    else if (!strcmp(args[arg], "-o")) {
+      if (!more) {
+        std::cerr << "missing argument to output flag" << std::endl;
         exit(__LINE__);
+      }
+
+      arg++;
+      if (!buffer) {
+        std::cerr << "no buffer loaded" << std::endl;
+        exit(__LINE__);
+      }
+      else if (strEndsWith(args[arg], ".fff.data")) {
+        FILE* fp = fopen(args[arg], "wb");
+        if (!fp) {
+          std::cerr << "unable to open " << args[arg] << std::endl;
+          exit(__LINE__);
+        }
+        int nFloats = w * h * c;
+        if (nFloats != fwrite(buffer, sizeof(float), nFloats, fp)) {
+          std::cerr << "fwrite() failure writing " << args[arg] << std::endl;
+          exit(__LINE__);
+        }
+        fclose(fp);
+        std::cerr << "wrote " << args[arg] << std::endl;
+      }
+      else if (strEndsWith(args[arg], ".jpg")) {
+        if (!stbi_write_jpg(args[arg], w, h, c, buffer, q)) {
+          std::cerr << "stbi_write_jpg() failure" << std::endl;
+          exit(__LINE__);
+        }
+        std::cerr << "wrote " << args[arg] << std::endl;
+      }
+      else {
+        std::cerr << "do not know how to write: " << args[arg] << std::endl;
+        exit(__LINE__);
+      }
     }
+    else if (!strcmp(args[arg], "-i")) {
+      if (!more) {
+        std::cerr << "missing argument to input flag" << std::endl;
+        exit(__LINE__);
+      }
 
-    int w, h;
-    int channels_in_file;
+      free(buffer);
+      buffer = NULL;
 
-    std::cout << "Checking channel count:";
-    if (!stbi_info_from_file(fp, &w, &h, &channels_in_file)) {
-      std::cerr << "stbi_info_from_file() failure" << std::endl;
-      exit(__LINE__);
+      arg++;
+      if (strEndsWith(args[arg], ".data") && w <= 0) {
+        std::cerr << "must set width before loading raw image" << std::endl;
+        exit(__LINE__);
+      }
+      else if (strEndsWith(args[arg], ".fff.data") || strEndsWith(args[arg], ".f.data")) {
+        //load RGB-FFF
+        c = strEndsWith(args[arg], ".fff.data") ? 3 : 1;
+        h = 0;
+
+        // --- Reading from a file (mode: "r" for read) ---
+        FILE* fp = fopen(args[arg], "rb");
+        if (fp == NULL) {
+          std::cerr << "Error opening file for reading:" << args[arg] << std::endl;
+          exit(__LINE__);
+        }
+
+        size_t r = w * c;
+        size_t sz = 0;
+        do {
+          while (sizeof(float) * w * c * (h + 1) > sz) {
+            sz = sz * 2 + sizeof(float) * w * c;
+            buffer = (float*)realloc(buffer, sz);
+            if (!buffer) {
+              std::cerr << "Out of memory" << std::endl;
+              exit(__LINE__);
+            }
+          }
+          r = fread(buffer + w * c * h, sizeof(float), w * c, fp);
+          h++;
+        } while (r == w * c);
+        if (r != 0) {
+          std::cerr << "Read " << r << " samples, which is not divisible into " << w << "*" << c << "samples per row" << std::endl;
+          std::cerr << "Please verify size(" << args[arg] << ") = width * height * channels * 4B" << std::endl;
+          exit(__LINE__);
+        }
+        h--;
+        std::cout << "Loaded " << args[arg] << " " << w << "x" << h << " c=" << c << std::endl;
+      }
+      else if (strEndsWith(args[arg], ".jpg")) {
+        std::cout << "Checking channel count:";
+        if (!stbi_info(args[arg], &w, &h, &c)) {
+          std::cerr << "stbi_info_from_file() failure:" << std::endl;
+          exit(__LINE__);
+        }
+        std::cout << c << std::endl;
+
+        std::cout << "Reading data from " << args[arg] << std::endl;
+        buffer = stbi_loadf(args[arg], &w, &h, &c, c);
+        if (!buffer) {
+          std::cerr << "stbi_loadf() failure: " << stbi_failure_reason() << std::endl;
+          exit(__LINE__);
+        }
+        std::cout << "Loaded " << args[arg] << " " << w << "x"<< h << " c=" << c << std::endl;
+      }
+      else {
+        std::cerr << "don't know how to load:" << args[arg] << std::endl;
+        exit(__LINE__);
+      }
     }
-    std::cout << channels_in_file << std::endl;
-
-    std::cout << "Reading data from file...";
-    desired_channels = channels_in_file;
-    float* pixels = stbi_loadf_from_file(fp, &w, &h, &channels_in_file, desired_channels);
-    if (!pixels) {
-      std::cerr << "stbi_loadf_from_file() failure" << std::endl;
-      exit(__LINE__);
+    else if (!strcmp(args[arg], "-s")) {
+      if (!buffer) {
+        std::cerr << "Image buffer empty" << std::endl;
+        exit(__LINE__);
+      }
+      stats(buffer, w * h, c);
     }
-    fclose(fp); // Close the file
-    std::cout << "done" << std::endl;
-
-    if (pixels) {
-      std::cout << "width:" << w << std::endl;
-      std::cout << "height:" << h << std::endl;
-
-      fp = fopen("output.fff.data", "wb");
-      assert(fp);
-      fwrite(pixels, sizeof(float), w * h * desired_channels, fp);
-      fclose(fp);
+    else if (!strcmp(args[arg], "-idct")) {
+      idct_test();
     }
     else {
-        std::cout << stbi_failure_reason() << std::endl;
-        return -1;
-    }
-
-    std::cout << "Unique pixels:";
-    qsort(pixels, w * h, sizeof(float) * desired_channels, fltVecCmp);
-    unsigned pcount = 1;
-    for (unsigned i = 1; i < w * h; ++i) {
-      const float* pixel = pixels + i * desired_channels;
-      pcount += !memcmp(pixel, pixel - desired_channels, sizeof(float) * desired_channels);
-    }
-    std::cout << pcount << std::endl;
-#endif
-
-#if 1
-    // --- Reading from a file (mode: "r" for read) ---
-    FILE* fp = fopen("input.fff.data", "rb");
-    if (fp == NULL) {
-      printf("Error opening file for reading!\n");
+      std::cerr << "did not understand arg: " << args[arg] << std::endl;
       exit(__LINE__);
     }
+  }
 
-    const int w = 1280;
-    const int h = 960;
-    const int comp = 3;
-    const int nPixels = w * h;
-    const int nFloats = nPixels * comp;
-
-    float* pixels = (float*)malloc(sizeof(float) * nFloats);
-    if(!pixels){
-      std::cerr << "malloc() failure" << std::endl;
-      exit(__LINE__);
-    }
-
-    if (nFloats != fread(pixels, sizeof(float), nFloats, fp)) {
-      std::cerr << "fread() failure" << std::endl;
-      exit(__LINE__);
-    }
-
-    if (!stbi_write_jpg("output.jpg", w, h, comp, pixels, 99)) {
-      std::cerr << "stbi_write_jpg() failure" << std::endl;
-      exit(__LINE__);
-    }
-    free(pixels);
-#endif
-
-    return 0;
+  return 0;
 }
